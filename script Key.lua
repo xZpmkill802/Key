47517af1ce04dd002d893c740929aaffbf18f133
xZpmkill802 <xzpmkillz@gmail.com>
2025-02-01 18:59:10 +0700

    Update script Key.lua

--- script Key.lua
+++ script Key.lua
@@ -1,5 +1,5 @@
 local key = _G.Key
-local check = "https://raw.githubusercontent.com/xZpmkill802/Key/refs/heads/main/Check.php" .. key
+local check = "https://raw.githubusercontent.com/xZpmkill802/Key/refs/heads/main/Check.php?key=" .. key
 if game:HttpGet(check) == "Whitelisted" then
 loadstring(game:HttpGet("https://raw.githubusercontent.com/xZpmkill802/Key/refs/heads/main/Key%20Sc.lua"))()
 else
